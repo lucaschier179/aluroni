cf9cfb28ad617de248235e21f302cc1c9fbc9885
lucaschier <ens-lucasschier@ugv.edu.br>
2023-04-25 10:37:02 -0300

    Aula - Fazendo os filtros funcionarem

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -1,6 +1,12 @@
 import styles from './Filtros.module.scss';
+import classNames from 'classnames';
 
 type IOpcao = typeof filtros[0];
+
+interface Props {
+    filtro: number | null;
+    setFiltro: React.Dispatch<React.SetStateAction<number | null>>;
+}
 
 const filtros = [
     {
@@ -21,18 +27,25 @@ const filtros = [
     }
 ]
 
-export default function Filtros() {
-
+export default function Filtros({ filtro, setFiltro }: Props) {
     function selecionarFiltro(opcao: IOpcao) {
-
+        if(filtro === opcao.id) return setFiltro(null);
+        return setFiltro(opcao.id);
     }
     return (
         <div className={styles.filtros}>
             {filtros.map((opcao) => (
-                <button className={styles.filtros__filtro} key={opcao.id} onClick={() => selecionarFiltro(opcao)}>
+                <button 
+                    className={classNames({
+                        [styles.filtros__filtro]: true,
+                        [styles['filtros__filtro--ativo']]: filtro === opcao.id
+                    })} 
+                    key={opcao.id} 
+                    onClick={() => selecionarFiltro(opcao)}
+                >
                     {opcao.label}
                 </button>
             ))}
         </div>
-    )
+    );
 }
