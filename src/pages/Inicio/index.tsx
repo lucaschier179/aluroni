19e1173bd1ace1522a12fce775a9e94d16486dfb
lucaschier <ens-lucasschier@ugv.edu.br>
2023-07-19 10:37:08 -0300

    Criando a página de Inicio

---
+++ src/pages/Inicio/index.tsx
@@ -0,0 +1,14 @@
+import Cardapio from "pages/Cardapio";
+
+export default function Inicio() {
+  return (
+    <section>
+      <h3>
+        Recomendações da cozinha
+      </h3>
+      <div>
+        <Cardapio />
+      </div>
+    </section>
+  );
+}
