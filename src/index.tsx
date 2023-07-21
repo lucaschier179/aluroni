6d6f52b9f3490f0d49ba922247eb7591884c8dff
lucaschier <ens-lucasschier@ugv.edu.br>
2023-07-21 15:00:30 -0300

    Começando rotas em js

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import Inicio from "pages/Inicio";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import Cardapio from "pages/Cardapio";
+
+const componenteAtual = window.location.pathname === "/" ? <Inicio /> : <Cardapio />;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Inicio />
+    {componenteAtual}
   </React.StrictMode>
 );
