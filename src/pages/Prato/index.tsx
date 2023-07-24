7b284b073f0a9e74680820fb0b267421f60d75ff
lucaschier <ens-lucasschier@ugv.edu.br>
2023-07-24 14:32:42 -0300

    Refatorando tags

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,9 +1,8 @@
-import classNames from "classnames";
+import TagsPrato from "components/TagsPrato";
 import cardapio from "data/cardapio.json";
 import { BiArrowBack } from "react-icons/bi";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import styles from "./Prato.module.scss";
-import { useNavigate } from "react-router-dom";
 
 export default function Prato() {
   const { id } = useParams();
@@ -30,23 +29,7 @@ export default function Prato() {
           <p className={styles.conteudo__descricao}>
             {prato.description}
           </p>
-          <div className={styles.tags}>
-            <div className={classNames({
-              [styles.tags__tipo]: true,
-              [styles[`tags__tipo__${prato.category.label.toLowerCase()}`]]: true
-            })}>
-              {prato.category.label}
-            </div>
-            <div className={styles.tags__porcao}>
-              {prato.size}g
-            </div>
-            <div className={styles.tags__qtdpessoas}>
-              Serve {prato.serving} pessoa{prato.serving === 1 ? "" : "s"}
-            </div>
-            <div className={styles.tags__valor}>
-              R$ {prato.price.toFixed(2)}
-            </div>
-          </div>
+          <TagsPrato {...prato} />
         </div>
       </section>
     </>
